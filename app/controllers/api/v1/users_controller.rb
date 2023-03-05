000048459632b05b82b9f487e4cc0c198ce49317
Brandon Gray <brandonjoe88@hotmail.com>
2023-03-05 10:28:44 -0700

    feat: randomize key in model

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
-    user.update(api_key: SecureRandom.hex(16))
+    user.update(api_key: User.random_key)
     if user.save
-      render json: { success: "user has been successfully created" }
+      render json: { success: "user has been successfully created" }, status: :created
     else
       raise CreateError.new(details: user.errors.full_messages.to_sentence)
     end
