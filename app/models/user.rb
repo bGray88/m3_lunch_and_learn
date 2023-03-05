180252e4abfab332e3d93da7a72af5e3064aeb4e
Brandon Gray <brandonjoe88@hotmail.com>
2023-03-05 10:42:30 -0700

    feat: adds favorite model, db, tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   validates :email, uniqueness: { case_sensitive: true }
   validates :api_key, uniqueness: { case_sensitive: true }
 
+  has_many :favorites, dependent: :destroy
+
   def self.random_key
     key = SecureRandom.hex(16)
     return SecureRandom.hex(16) if User.find_by(api_key: key)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,5 +8,7 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:api_key) }
     it { should validate_presence_of(:api_key) }
+
+    it { should have_many(:favorites)}
   end
 end
